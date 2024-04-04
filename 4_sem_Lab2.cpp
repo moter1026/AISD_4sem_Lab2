97fbbe230608d15eaa50a475c9e3459e2d59422b
moter1026 <matvej.sidoroff2016@yandex.ru>
2024-04-05 03:01:23 +0400

    add void insert_or_assign(K key, V Value), bool contains(V value), 10. V* search(K key), bool erase(K key), int count(K key)

--- 4_sem_Lab2.cpp
+++ 4_sem_Lab2.cpp
@@ -9,24 +9,62 @@ using namespace std;
 
 int main()
 {
-	My_unordered_map<int> My_class(15);
+	setlocale(LC_ALL, "ru");
+	My_unordered_map<int, int> My_class(15);
 
-	My_class.ptint();
+	My_class.print();
+	try {
+		My_class.insert(124, 1000);
+		My_class.insert(555, 2000);
+		My_class.insert(333, 3000);
+		My_class.insert(111, 4000);
+		My_class.insert(111, 5000);
+		My_class.insert(555, 6000);
+		My_class.insert(111, 7000);
 
-	My_class.insert(124, 1000);
-	My_class.insert(555, 2000);
-	My_class.insert(333, 3000);
-	My_class.insert(111, 4000);
-	My_class.insert(111, 5000);
-	My_class.insert(111, 6000);
-	My_class.insert(124, 7000);
-	My_class.insert(124, 8000);
-	My_class.insert(124, 9000);
-	My_class.insert(124, 10000);
-	My_class.insert(124, 11000);
-	My_class.insert(555, 12000);
-	My_class.insert(555, 12000);
-	My_class.ptint();
+	
+		My_class.print();
 
-	return 0;
+		/*My_class.insert(-111, 5000);
+		My_class.print();*/
+
+		std::cout << My_class.contains(2000) << "\n" <<
+			My_class.contains_key(555) << "\n" <<
+			My_class.contains(555, 2000) << std::endl;
+
+		std::cout << GREEN_TEXT << "\n" << *My_class.search(333) << RESET_TEXT << std::endl;
+
+		My_class.erase(555);
+		std::cout << "После удаления эл-та" << std::endl;
+		My_class.print();
+
+		std::cout << YELLOW_TEXT << "Кол-во эл-тов с ключом 111 = " << My_class.count(111) << RESET_TEXT << std::endl;
+		
+
+		My_unordered_map<int, int> My_class2;
+		My_class2 = My_class;
+
+		std::cout << YELLOW_TEXT << "Приравниваем My_class2 к My_class через оператор ="  << RESET_TEXT << std::endl;
+		std::cout << YELLOW_TEXT << "My_class2 :" << RESET_TEXT << std::endl;
+		My_class2.print();
+
+		My_class2.erase(555);
+		std::cout << "После удаления эл-та в My_class2. My_class2:" << std::endl;
+		My_class2.print();
+		std::cout << "После удаления эл-та в My_class2. My_class:" << std::endl;
+		My_class.print();
+
+		std::cout << "\n\n";
+		std::cout << YELLOW_TEXT << "Пытаюсь вставить эл-нт через метод insert_or_assign" << RESET_TEXT << std::endl;
+		My_class.insert_or_assign(444, 4444);
+		My_class.print();
+		std::cout << "\n";
+		std::cout << YELLOW_TEXT << "Теперь метод insert_or_assign должен заменить эл-нт с существующим ключём" << RESET_TEXT << std::endl;
+		My_class.insert_or_assign(444, 5555);
+		My_class.print();
+		return 0;
+	}
+	catch (std::runtime_error err) {
+		std::cout << std::string(RED_TEXT) << err.what() << std::string(RESET_TEXT);
+	}
 }
--- 4_sem_Lab2.h
+++ 4_sem_Lab2.h
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
+#include <exception>
 
 
 const char* BLACK_TEXT = "\033[30m";
@@ -18,16 +19,20 @@ const char* GRAY_TEXT = "\033[37m";
 const char* RESET_TEXT = "\033[0m";
 
 
-template<typename T>
+template<typename K, typename V>
 class My_unordered_map {
-	template<typename M>
 	class Pair {
-		int _key;
-		M _value;
+		K _key;
+		V _value;
 
 	public:
-		int getKey() const { return this->_key; }
-		M getValue() const { return this->_value; }
+		K& getKey() { return this->_key; }
+		V& getValue() { return this->_value; }
+		K getKey() const noexcept { return this->_key; }
+		V getValue() const noexcept { return this->_value; }
+
+		void setKey(K key) { this->_key = key;}
+		void setValue(V value) { this->_value = value; }
 
 		Pair& operator=(const Pair& obj_1) {
 			this->_key = obj_1.getKey();
@@ -37,42 +42,66 @@ class My_unordered_map {
 		}
 
 		Pair() : _key(-1), _value(0) {};
-		Pair(int key, M value) : _key(key), _value(value) {};
+		Pair(K key, V value) : _value(value) {
+			if (key < 0) throw std::runtime_error("Ключ не может быть меньше нуля");
+			this->_key = key;
+		};
 		Pair(const Pair& obj) {
 			this->_key = obj.getKey();
 			this->_value = obj.getValue();
 		}
-		~Pair() {
-			this->_key = 0;
-			this->_value = 0;
-		}
+		~Pair() = default;
 
-		friend std::ostream& operator<<(std::ostream& stream, const Pair<M>& pair) {
+		friend std::ostream& operator<<(std::ostream& stream, const Pair& pair) {
 			stream << "(" << pair.getKey() << "," << pair.getValue() << ")";
 			return stream;
 		}
 	};
 	
-	
-
 	int A = 677;
 	// Определяю размер машинного слова
 	size_t MACHINE_WORD = sizeof(size_t);
-
-	Pair<T>* _arr_pair;
+	Pair* _arr_pair;
 	size_t _size;
 
+	// Возвращает индекс первого эл-та, имеющий ключ key
+	size_t at(K key) const{
+		checkKey(key);
+
+		size_t index = this->hash(key);
+
+		for (size_t i = index; i < this->getSize(); ++i)
+		{
+			if (this->_arr_pair[i].getKey() == key) {
+				return i;
+			}
+		}
+		for (size_t i = 0; i < index; ++i)
+		{
+			if (this->_arr_pair[i].getKey() == key) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	void checkKey(K key) const {
+		if (key >= 0) return;
+		throw std::runtime_error("Ключ не должен быть меньше нуля");
+	}
 public:
-	size_t getSize() const { return this->_size; }
-	Pair<T> getPair(size_t index) const { return this->_arr_pair[index]; }
-	int hash(int key) {
+	size_t getSize() const noexcept { return this->_size; }
+	Pair& getPair(size_t index) const noexcept { return this->_arr_pair[index]; }
+	int hash(int key) const {
 		double n;
 
 		double A_machine_word_and_key = (double(this->A) / this->MACHINE_WORD * key);
 		double modf = std::modf(A_machine_word_and_key, &n);
 		return std::floor(this->_size * (modf));
 	}
-	void ptint() const{
+	/*int hash(std::string rome_number) const {
+		
+	}*/
+	void print() const noexcept {
 		std::cout << GREEN_TEXT << "Hash table with size = " << this->getSize() << " and method 'open andress'." << RESET_TEXT <<std::endl;
 		
 		// Заголовки колонок
@@ -88,7 +117,64 @@ public:
 				<< std::setw(1) << std::left << this->getPair(i) << std::endl;
 		}
 	}
-	void insert(int key, T value) {
+
+	bool contains(K value) const {
+		for (size_t i = 0; i < this->getSize(); ++i)
+		{
+			// Проверка на равенство значений и на то, чтобы ключ был валидным
+			if (this->_arr_pair[i].getValue() == value && 
+				this->_arr_pair[i].getKey() != -1) {
+				return true;
+			}
+		}
+		return false;
+	}
+	bool contains_key(int key) const {
+		checkKey(key);
+
+		if (this->at(key) != -1) return true;
+		return false;
+	}
+	bool contains(int key, K value) const {
+		checkKey(key);
+
+		int index = this->hash(key);
+
+		if (this->_arr_pair[index].getKey() == -1) {
+			return false;
+		}
+
+		// Начинаем поиск с index, потому что более вероятно, что нужное
+		for (size_t i = index; i < this->getSize(); ++i)
+		{
+			if (this->_arr_pair[i].getKey() == key &&
+				this->_arr_pair[i].getValue() == value) {
+				return true;
+			}
+		}
+		for (size_t i = 0; i < index; ++i)
+		{
+			if (this->_arr_pair[i].getKey() == key &&
+				this->_arr_pair[i].getValue() == value) {
+				return true;
+			}
+		}
+		return false;
+	}
+	V* search(K key) const {
+		checkKey(key);
+		size_t index = this->at(key);
+		if (index != -1) 
+		{
+			V& value = this->getPair(index).getValue();
+			return &value;
+		}
+		return nullptr;
+	}
+
+	void insert(int key, K value) {
+		checkKey(key);
+
 		int index = this->hash(key);
 
 		for (size_t i = index; i < this->getSize(); ++i)
@@ -105,13 +191,64 @@ public:
 				return;
 			}
 		}
+		return;
 	}
+	// Вставляет эл-т, если до этого не было эл-тов с таким ключём.
+	// Иначе заменяет у существующего эл-та значение value
+	void insert_or_assign(K key, V value) {
+		checkKey(key);
 
+		size_t index = this->at(key);
+		
+		if (index == -1) {
+			index = this->hash(key);
+
+			for (size_t i = index; i < this->getSize(); ++i)
+			{
+				if (this->_arr_pair[i].getKey() == -1) {
+					this->_arr_pair[i] = Pair(key, value);
+					return;
+				}
+			}
+			for (size_t i = 0; i < index; ++i)
+			{
+				if (this->_arr_pair[i].getKey() == -1) {
+					this->_arr_pair[i] = Pair(key, value);
+					return;
+				}
+			}
+		}
+		this->getPair(index).setValue(value);
+	}
+	bool erase(K key) {
+		size_t index = this->at(key);
+
+		if (index != -1)
+		{
+			this->getPair(index).setKey(-1);
+			this->getPair(index).setValue(0);
+			return true;
+		}
+		return false;
+	}
+	int count(K key) const {
+		checkKey(key);
+
+		int count = 0;
+		for (size_t i = 0; i < this->getSize(); ++i)
+		{
+			if (this->_arr_pair[i].getKey() == key) {
+				++count;
+			}
+		}
+		return count;
+	}
+	
 	My_unordered_map& operator=(const My_unordered_map& obj) {
 		this->_size = obj.getSize();
-		this->_arr_pair = new Pair<T>[this->_size];
+		this->_arr_pair = new Pair[this->_size];
 
-		for (size_t i = 0; i < this->_size - 1; ++i)
+		for (size_t i = 0; i < this->_size; ++i)
 		{
 			this->_arr_pair[i] = obj.getPair(i);
 		}
@@ -119,25 +256,24 @@ public:
 		return *this;
 	}
 
-	My_unordered_map() = default;
+	My_unordered_map() {
+		this->_size = 10;
+		this->_arr_pair = new Pair[this->_size];
+	};
 	My_unordered_map(size_t size): _size(size) {
-		this->_arr_pair = new Pair<T>[size];
+		this->_arr_pair = new Pair[size];
 	}
 	My_unordered_map(const My_unordered_map& obj) {
 		this->_size = obj.getSize();
-		this->_arr_pair = new Pair<T>[this->_size];
+		this->_arr_pair = new Pair[this->_size];
 
-		for (size_t i = 0; i < this->_size - 1; ++i)
+		for (size_t i = 0; i < this->_size; ++i)
 		{
 			this->_arr_pair[i] = obj.getPair(i);
 		}
 	}
 
 	~My_unordered_map() {
-		this->A = 0;
-		this->MACHINE_WORD = 0;
-		this->_size = 0;
-
 		delete[] this->_arr_pair;
 	}
 };
